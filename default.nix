4102905f6404648024a7cd4f762ea53597aa6430
PJ <pj@imagine-nyc.com>
2020-04-30 23:28:55 +0200

    default.nix: update cargoSha256

--- default.nix
+++ default.nix
@@ -8,7 +8,7 @@ with pkgs;
 nixpkgs.rustPlatform.buildRustPackage {
   name = "holo-update-conductor-config";
   src = gitignoreSource ./.;
-  cargoSha256 = "0kwa3dmnqcljbkksj1zajnkhilrx3ps8rya7qyisv5zs540ipkc2";
+  cargoSha256 = "10ndvvv3q49pjsffk17zzjlvq51prkskcdywv5hjaqf9h5rmzmnj";
 
   meta.platforms = lib.platforms.linux;
 }
